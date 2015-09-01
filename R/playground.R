e2ee865f42763487629d16d5365bb5e17d032da4
Piotr Mankowski <pmankowski@partners.org>
2015-09-01 11:32:25 -0400

    Paper focus setup

--- R/playground.R
+++ R/playground.R
@@ -2,7 +2,12 @@ source("R/sources.R")
 source("R/plotting/rasters/raster_plot.R")
 source("R/analysis/analysis.R")
 
-load_data(local=FALSE)
+
+fd_info_2015a <- as.data.table(read.xls("/I/Projects/Forced Desynchrony data projects/FD-info 2015a.xls"))
+
+
+
+subjects <- read_subject_info(subject_fp.all)
 setup_episodes(sleep_data, sleep_data)
 setup_cycles(sleep_data, episodes)
 setup_raster_data(sleep_data, episodes, cycles)
