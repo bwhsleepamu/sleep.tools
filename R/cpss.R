0ad73788dfc0ae08206bd42d8fd87560c428c17a
Piotr Mankowski <pmankowski@partners.org>
2015-10-05 17:07:29 -0400

    Output of sleep schedule

--- R/cpss.R
+++ R/cpss.R
@@ -11,8 +11,11 @@ NUMBER_OF_DAYS <- NA
 FIRST_DAY <- 1
 COLOR_PALETTE <- c("#E69F00", "#56B4E9", "#009E73", "#0072B2", "#F0E442", "#888888", "#000000", "#D55E00",  "#CC79A7", "#555555")
 
-main_data <<- as.data.table(read.csv("~/Desktop/cpss/main_raster_data_23.90.csv"))
-cbt_data <<- as.data.table(read.csv("~/Desktop/cpss/cbt_min_23.90.csv"))
+main_data <<- as.data.table(read.csv("~/Desktop/cpss/main_raster_data_24.30.csv"))
+cbt_data <<- as.data.table(read.csv("~/Desktop/cpss/cbt_min_24.30.csv"))
 
 setup_raster_data()
 plot_raster()
+
+
+write.csv()
