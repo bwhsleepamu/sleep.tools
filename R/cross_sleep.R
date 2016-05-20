38d8f4d7f8e3fe13a9c9182f0ce3a8297b8130da
Piotr Mankowski <pmankowski@partners.org>
2016-05-20 16:34:08 -0400

    Small change

--- R/cross_sleep.R
+++ R/cross_sleep.R
@@ -1,5 +1,6 @@
 to_analyze <- as.data.table(read.csv("/home/pwm4/Desktop/a1/FD-info for Cross Slp.csv"))
 View(to_analyze)
 to_analyze <- to_analyze[1:101,,which=FALSE];
-to_analyze
+to_analyze[,Subject:=as.character(Subject)];
 
+to_analyze$Subject %in% 
